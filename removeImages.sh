892f328f9ca7bff3bee2d0261aaf91a3adf68780
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2023-01-13 11:15:07 +0100

    images 2022

--- removeImages.sh
+++ removeImages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IFS=$'\n'; set -f
 #for f in $(find assets/img/2023 -type f -print | grep "x"); do echo "$f"; done
-for f in $(find assets/img/2023 -type f -print | grep "x"); do rm "$f"; done
+#for f in $(find assets/img/2023 -type f -print | grep "x"); do rm "$f"; done
 for f in $(find assets/img/2022 -type f -print | grep "x"); do rm "$f"; done
 
 
