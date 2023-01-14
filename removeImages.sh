f3dfb20dc6a60679f626505290462b45240e65e4
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2023-01-14 12:25:36 +0100

    images 2018

--- removeImages.sh
+++ removeImages.sh
@@ -2,7 +2,7 @@
 IFS=$'\n'; set -f
 #for f in $(find assets/img/2023 -type f -print | grep "x"); do echo "$f"; done
 #for f in $(find assets/img/2023 -type f -print | grep "x"); do rm "$f"; done
-for f in $(find assets/img/2019 -type f -print | grep "x"); do rm "$f"; done
+for f in $(find assets/img/2018 -type f -print | grep "x"); do rm "$f"; done
 
 
 unset IFS; set +f
